2b02b9c13b4cfdd1438750f27d95868f2f84b992
duyhungg <dongocduyhung7@gmail.com>
2025-03-04 21:35:34 +0700

    update pipe validate

---
+++ src/users/dtos/create-user.dtos.ts
@@ -0,0 +1,35 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+    message:
+      'Minimum eight characters, at least one letter, one number and one special character',
+  })
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,42 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   Header,
-  Headers,
-  Ip,
   Param,
+  ParseIntPipe,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dtos';
+import { GetUsersParamsDto } from './dtos/get-user-params.dtos';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log('params ', id);
+  public getUsers(
+    @Param() getUserParamsDto: GetUsersParamsDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log('getUserParamsDto ', getUserParamsDto);
     console.log('query ', limit);
+    console.log('page ', page);
 
     return `123`;
   }
   @Post()
   @Header('Cache-Control', 'no-store')
-  public createUsers(
-    @Body() request: any,
-    @Headers() header: any,
-    @Ip() ip: any,
-  ) {
-    console.log('request', request);
-    console.log('header', header);
-    console.log('ip', ip);
+  public createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log('request', createUserDto instanceof CreateUserDto);
     return 'create user';
+  }
+  @Patch()
+  public patchUser(@Body() pathUserDto: PatchUserDto) {
+    console.log('pathUserDto', pathUserDto);
+    return 'path';
   }
 }
