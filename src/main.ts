363681bd5accaeba05e971c2b17864d58afaed06
duyhungg <dongocduyhung7@gmail.com>
2025-04-11 09:54:58 +0700

    update swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -11,6 +11,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Blog app')
+    .setDescription('app intro nesst js')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/api/')
+    .build();
+  // init
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
